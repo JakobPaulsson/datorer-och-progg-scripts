16d1d0dbff51e5a77b381d528115ce7e8574618e
Jakob Paulsson <jakob.paulsson123@gmail.com>
2022-06-16 17:43:43 +0200

    Konverterade inlupp3 bat till shell

---
+++ inlupp3/uppgift3_script.sh
@@ -0,0 +1,13 @@
+if [ ! -f "$1" ]; then
+    echo "Filen $1 finns ej."
+    exit 0
+fi
+g++ -o uppgift3_losning $1
+for i in {1..7}
+do
+    echo Testdata $i:
+    echo
+    ./uppgift3_losning < inputs/uppgift3_inputs_$i.txt
+    echo
+done
+rm uppgift3_losning
